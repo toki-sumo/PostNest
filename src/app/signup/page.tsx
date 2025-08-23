3b6dc393d7d87aca63e8f6b2064f68e58b9cd458
tokisumo <mhc4pd8@gmail.com>
2025-08-23 13:27:05 +0900

    signup comment

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -186,6 +186,7 @@ const SignupPage = () => {
                                         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 15v2m-6 4h12a2 2 0 002-2v-6a2 2 0 00-2-2H6a2 2 0 00-2 2v6a2 2 0 002 2zm10-10V7a4 4 0 00-8 0v4h8z" />
                                     </svg>
                                     パスワード
+                                    <span className="text-xs text-gray-500 ml-1">（英大文字・小文字・数字・特殊文字を含めた８文字以上にしてください）</span>
                                 </label>
                                 <Input
                                     type="password"
